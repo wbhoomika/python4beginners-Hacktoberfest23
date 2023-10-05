7ff17458f2f203b5411d522f5bd2c029241ea525
Pinak01 <pinak2106debnath@gmail.com>
2023-10-05 12:55:40 +0530

    Improved version

--- Python Projects/guess the no.py
+++ Python Projects/guess the no.py
@@ -1,13 +1,29 @@
 import random
 
-r=random.randint(0,100)
+def guess_the_number():
+    # Generate a random number between 1 and 100
+    r = random.randint(1, 100)
+    attempts = 0
 
-while(1):
-    n=int(input("enter the guess:"))
-    if(n==r):
-        print("Congratulations you guess correct:",n)
-        break
-    if(n>r):
-       print("guess is high......... Try again")
-    else:
-        print("guess is low..........Try again")
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100. Can you guess it?")
+
+    while True:
+        try:
+            n = int(input("Enter your guess: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+
+        attempts += 1
+
+        if n == r:
+            print(f"Congratulations! You guessed the correct number {n} in {attempts} attempts.")
+            break
+        elif n > r:
+            print("Your guess is too high. Try again.")
+        else:
+            print("Your guess is too low. Try again.")
+
+if __name__ == "__main__":
+    guess_the_number()
